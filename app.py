c5d98dd19fd6b5ebd05fd882fa74830456997706
buidung2004 <66249109+buidung2004@users.noreply.github.com>
2021-03-18 15:27:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -130,5 +130,5 @@ if __name__ == '__main__':
     # app.run(port=5002, threaded=False)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server = WSGIServer(('0.0.0.0', 8080), app)
     http_server.serve_forever()
